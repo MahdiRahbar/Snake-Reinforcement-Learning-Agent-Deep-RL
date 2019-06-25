f345eabe3947308aac39efae3e7f2aaa77771cd6
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2019-06-26 00:09:43 +0430

    added requirement libraries

--- learn.py
+++ learn.py
@@ -1,1 +1,11 @@
 import snake as game
+import argparse
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Convolution2D
+from keras.optimizers import Adam
+import numpy as np
+import skimage as skimage
+import random
+import json
+from random import sample as rsample
+import time
