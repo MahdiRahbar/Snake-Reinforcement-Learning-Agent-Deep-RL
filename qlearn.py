4c06d5940780838b0d58d9bfb0ec04de7f2fda8e
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2019-07-01 17:53:56 +0430

    Added the initial parameters

--- qlearn.py
+++ qlearn.py
@@ -9,3 +9,14 @@ import random
 import json
 from random import sample as rsample
 import time
+
+INPUT_SHAPE = (80, 80, 2)  # Shape of the image imported into the NN
+NB_ACTIONS = 5  # NB_ACTIONS is the number of actions the player can do
+BATCH = 100
+GAME_INPUT = [0, 1, 2, 3, 4]
+EPSILON = 1
+EPSILON_DECAY = 0.99
+FINAL_EPSILON = 0.3
+LEARNING_RATE = 1e-4
+GAMMA = 0.7
+NB_FRAMES = 1
